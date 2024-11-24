e6faff96de920af338074cb39cecfdca80d2d647
Raquel Natale <raquel.natali95@hotmail.com>
2024-11-24 18:26:44 -0300

    Resolvendo problemas de concorrencia

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,15 +1,18 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
+const pause = (duration: number) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
+};
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async(isolatedClient) => {
-
-		await isolatedClient.watch(itemsKey(attrs.itemId));
-	
+	return withLock(attrs.itemId, async(lockedClient: typeof client, signal: any) => {
 		const item = await getItem(attrs.itemId);
+		// await pause(5000);
 
 		if (!item) {
 			throw new Error('Item not found');
@@ -24,24 +27,23 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 		}
 
 		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
-
-		return isolatedClient.multi().
-			rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id),{
+		if (signal.expired){
+			throw new Error("Lock expired, cant write any more data")
+		}
+		return Promise.all([
+			lockedClient.rPush(bidHistoryKey(attrs.itemId), serialized),
+			lockedClient.hSet(itemsKey(item.id),{
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId,
-			})
-			.zAdd(itemsByPriceKey(), {
+			}),
+			lockedClient.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
-			})
-			.exec();
+			})]);
 
-	})
-
-	
-};
+	});
+}
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	const startIndex = -1 * offset - count;
